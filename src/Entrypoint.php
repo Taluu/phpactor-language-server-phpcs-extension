53f6cd3d0a8b8dadf243558031bf04e72866ac6b
Baptiste Clavié <clavie.b@gmail.com>
2020-12-21 13:09:16 +0100

    Init

---
+++ src/Entrypoint.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phpactor\Extension\LanguageServerPhpCs;
+
+use Phpactor\Container\ContainerBuilder;
+use Phpactor\Container\Extension;
+use Phpactor\MapResolver\Resolver;
+
+final class Entrypoint implements Extension
+{
+    public function load(ContainerBuilder $container)
+    {
+    }
+
+    public function configure(Resolver $schema)
+    {
+    }
+}
