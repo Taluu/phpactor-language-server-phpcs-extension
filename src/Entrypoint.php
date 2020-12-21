c4a233c23b2caf15b511d03c8b5b0f40179a8cec
Baptiste Clavié <clavie.b@gmail.com>
2020-12-21 15:05:43 +0100

    Add basic configuration

--- src/Entrypoint.php
+++ src/Entrypoint.php
@@ -10,11 +10,18 @@ use Phpactor\MapResolver\Resolver;
 
 final class Entrypoint implements Extension
 {
+    private const PHPCS_BINARY_PATH_PARAM = 'language_server.phpcs.bin';
+    private const PHPCS_STANDARD_PARAM = 'language_server.phpcs.standard';
+
     public function load(ContainerBuilder $container)
     {
     }
 
     public function configure(Resolver $schema)
     {
+        $schema->setDefaults([
+            self::PHPCS_BINARY_PATH_PARAM => '%project_root%/vendor/bin/phpcs',
+            self::PHPCS_STANDARD_PARAM => null,
+        ]);
     }
 }
